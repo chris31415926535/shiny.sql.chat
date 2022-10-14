46ad503e52f9ff6e932b3079c82c36b2467a5f64
Christopher Belanger <christopher@belangeranalytics.com>
2022-10-14 12:06:49 -0400

    Move JS to separate file

--- app.R
+++ app.R
@@ -23,36 +23,6 @@ ui <- fluidPage(
     tags$link(rel = "stylesheet", type = "text/css", href = "chat_styling.css")
   ),
 
-
-
-  # javascript from https://github.com/Appsilon/shiny.collections/blob/master/inst/examples/www/script.js
-#   tags$script(HTML("
-#   jQuery(document).ready(function(){
-#   jQuery('#msg_text').keypress(function(evt){
-#     if (evt.keyCode == 13){
-#       // Enter, simulate clicking send
-#       jQuery('#msg_button').click();
-#     }
-#   });
-# })
-#
-# // this autoscroll doesn't work though
-# // Scrolling down
-# var oldContent = null;
-# window.setInterval(function() {
-#   var elem = document.getElementById('chat-container');
-#   if (oldContent != elem.innerHTML){
-#     scrollToBottom();
-#   }
-#   oldContent = elem.innerHTML;
-# }, 300);
-#
-# function scrollToBottom(){
-#   var elem = document.getElementById('chat-container');
-#   elem.scrollTop = elem.scrollHeight;
-# }
-#                    ")),
-
   # Application title
   titlePanel("Simple SQL-Powered Chat in R Shiny!"),
 
